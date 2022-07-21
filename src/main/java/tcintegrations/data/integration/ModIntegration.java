b4ef0dea63da0c62cb4019df2e66b41376504549
Wendall Cada <wendallc@83864.com>
2022-07-21 16:00:25 -0700

    Don't mock aquaculture items, use direct reference.

--- src/main/java/tcintegrations/data/integration/ModIntegration.java
+++ src/main/java/tcintegrations/data/integration/ModIntegration.java
@@ -17,9 +17,6 @@ public final class ModIntegration extends TCIntegrationsModule {
     public static final String AQUACULTURE_MODID = "aquaculture";
 
     public static Item BOTANIA_LIVINGWOOD_PLANKS;
-    public static Item AQUACULTURE_NEPTUNIUM_INGOT;
-    public static Item AQUACULTURE_TIN_CAN;
-    public static Item AQUACULTURE_REDSTONE_HOOK;
 
     public static IForgeRegistry<Item> ITEM_REGISTRY;
 
@@ -30,9 +27,6 @@ public final class ModIntegration extends TCIntegrationsModule {
 
         if (dataGen != null && dataGen.contains("all")) {
             BOTANIA_LIVINGWOOD_PLANKS = registerItem(botaniaLoc("livingwood_planks"));
-            AQUACULTURE_NEPTUNIUM_INGOT = registerItem(aquacultureLoc("neptunium_ingot"));
-            AQUACULTURE_TIN_CAN = registerItem(aquacultureLoc("tin_can"));
-            AQUACULTURE_REDSTONE_HOOK = registerItem(aquacultureLoc("redstone_hook"));
         }
 
     }
@@ -47,18 +41,6 @@ public final class ModIntegration extends TCIntegrationsModule {
 
     public static ResourceLocation botaniaLoc(String name) {
         return getLoc(BOTANIA_MODID, name);
-    }
-
-    public static ResourceLocation tconLoc(String name) {
-        return getLoc(TCON_MODID, name);
-    }
-
-    public static ResourceLocation ieLoc(String name) {
-        return getLoc(IE_MODID, name);
-    }
-
-    public static ResourceLocation aquacultureLoc(String name) {
-        return getLoc(AQUACULTURE_MODID, name);
     }
 
     private static ResourceLocation getLoc(String modid, String name) {
