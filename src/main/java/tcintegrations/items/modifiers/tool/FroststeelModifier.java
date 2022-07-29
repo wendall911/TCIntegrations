ad1c4579d1d25039968ffc7284b547c13caa1c5a
Wendall Cada <wendallc@83864.com>
2022-07-29 08:14:07 -0700

    Change tag check for Froststeel modifier.

--- src/main/java/tcintegrations/items/modifiers/tool/FroststeelModifier.java
+++ src/main/java/tcintegrations/items/modifiers/tool/FroststeelModifier.java
@@ -3,7 +3,6 @@ package tcintegrations.items.modifiers.tool;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
 
 import slimeknights.tconstruct.common.TinkerTags;
 import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
@@ -18,11 +17,10 @@ public class FroststeelModifier extends NoLevelsModifier {
         int amplifier = 1;
 
         if (target != null) {
-            ItemStack toolStack = new ItemStack(tool.getItem());
-
-            if (toolStack.is(TinkerTags.Items.MELEE)) {
+            if (tool.hasTag(TinkerTags.Items.MELEE)) {
                 amplifier = 2;
             }
+
             target.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 600, amplifier));
         }
 
