a3e9c337bfe40f401bd157764440eb3b440fc237
Wendall Cada <wendallc@83864.com>
2023-06-10 15:16:05 -0700

    Revert change to latest TCon.

--- src/main/java/tcintegrations/items/modifiers/armor/EngineersGogglesModifier.java
+++ src/main/java/tcintegrations/items/modifiers/armor/EngineersGogglesModifier.java
@@ -1,26 +1,23 @@
 package tcintegrations.items.modifiers.armor;
 
 import slimeknights.tconstruct.common.TinkerTags;
-import slimeknights.tconstruct.library.modifiers.Modifier;
-import slimeknights.tconstruct.library.modifiers.ModifierEntry;
-import slimeknights.tconstruct.library.modifiers.hook.build.RawDataModifierHook;
 import slimeknights.tconstruct.library.modifiers.impl.NoLevelsModifier;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 import slimeknights.tconstruct.library.utils.RestrictedCompoundTag;
 
-public class EngineersGogglesModifier extends NoLevelsModifier implements RawDataModifierHook {
+public class EngineersGogglesModifier extends NoLevelsModifier {
 
     public static final String CREATE_GOGGLES = "create_goggles";
 
     @Override
-    public void addRawData(IToolStackView tool, ModifierEntry modifier, RestrictedCompoundTag tag) {
+    public void addRawData(IToolStackView tool, int level, RestrictedCompoundTag tag) {
         if (tool.hasTag(TinkerTags.Items.HELMETS)) {
             tag.putBoolean(CREATE_GOGGLES, true);
         }
     }
 
     @Override
-    public void removeRawData(IToolStackView tool, Modifier modifier, RestrictedCompoundTag tag) {
+    public void beforeRemoved(IToolStackView tool, RestrictedCompoundTag tag) {
         tag.remove(CREATE_GOGGLES);
     }
 
