bd11baa97ea734c1feb5e7f0e4f9f8533182ca97
Wendall Cada <wendallc@83864.com>
2024-05-25 10:26:47 -0700

    Add JEI support.

---
+++ src/main/java/tcintegrations/client/integration/jei/JEIPlugin.java
@@ -0,0 +1,53 @@
+package tcintegrations.client.integration.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.forge.ForgeTypes;
+import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.api.runtime.IJeiRuntime;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.material.Fluid;
+
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidType;
+import net.minecraftforge.registries.tags.ITag;
+
+import tcintegrations.data.tcon.SmelteryCompat;
+
+import java.util.Collections;
+
+import static tcintegrations.util.ResourceLocationHelper.location;
+import static tcintegrations.util.ResourceLocationHelper.resource;
+import static tcintegrations.util.TagHelper.getTag;
+
+@JeiPlugin
+public class JEIPlugin implements IModPlugin {
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return resource("jei_plugin");
+    }
+
+    @Override
+    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+        IIngredientManager manager = jeiRuntime.getIngredientManager();
+
+        for (SmelteryCompat compat : SmelteryCompat.values()) {
+            ITag<Item> ingot = getTag(location("forge", "ingots/" + compat.getName()));
+
+            if (ingot.isEmpty()) {
+                removeFluid(manager, compat.getFluid().get(), compat.getBucket());
+            }
+        }
+    }
+
+    private static void removeFluid(IIngredientManager manager, Fluid fluid, Item bucket) {
+        manager.removeIngredientsAtRuntime(ForgeTypes.FLUID_STACK, Collections.singleton(new FluidStack(fluid, FluidType.BUCKET_VOLUME)));
+        manager.removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK, Collections.singleton(new ItemStack(bucket)));
+    }
+
+}
