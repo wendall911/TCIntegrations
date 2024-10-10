fcaffeba8ef6197e32680c071a02f75fc524f558
Wendall Cada <wendallc@83864.com>
2024-10-10 09:43:11 -0700

    Add missing bronze block/ingot/nugget conversion recipes. Fixes #69

--- src/main/java/tcintegrations/data/recipes/ModRecipesProvider.java
+++ src/main/java/tcintegrations/data/recipes/ModRecipesProvider.java
@@ -5,10 +5,16 @@ import java.util.function.Consumer;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Ingredient;
 
 import net.minecraftforge.common.crafting.conditions.ICondition;
 
 import slimeknights.mantle.recipe.data.ConsumerWrapperBuilder;
+
+import tcintegrations.TCIntegrations;
+import tcintegrations.items.TCIntegrationsItems;
 
 public class ModRecipesProvider extends RecipeProvider {
 
@@ -24,6 +30,26 @@ public class ModRecipesProvider extends RecipeProvider {
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
         Consumer<FinishedRecipe> wrapped;
+
+        ShapelessRecipeBuilder.shapeless(TCIntegrationsItems.BRONZE.getNugget(), 9)
+            .requires(TCIntegrationsItems.BRONZE.getIngotTag())
+            .unlockedBy("has_bronze_ingot", has(TCIntegrationsItems.BRONZE.getIngotTag()))
+            .save(consumer, new ResourceLocation(TCIntegrations.MODID, "bronze_ingot_from_nuggets"));
+
+        ShapelessRecipeBuilder.shapeless(TCIntegrationsItems.BRONZE.getIngot(), 1)
+            .requires(Ingredient.of(TCIntegrationsItems.BRONZE.getNuggetTag()), 9)
+            .unlockedBy("has_bronze_nugget", has(TCIntegrationsItems.BRONZE.getNuggetTag()))
+            .save(consumer, new ResourceLocation(TCIntegrations.MODID, "bronze_nuggets_from_ingot"));
+
+        ShapelessRecipeBuilder.shapeless(TCIntegrationsItems.BRONZE.get(), 1)
+            .requires(Ingredient.of(TCIntegrationsItems.BRONZE.getIngotTag()), 9)
+            .unlockedBy("has_bronze_ingot", has(TCIntegrationsItems.BRONZE.getIngotTag()))
+            .save(consumer, new ResourceLocation(TCIntegrations.MODID, "bronze_block_from_ingots"));
+
+        ShapelessRecipeBuilder.shapeless(TCIntegrationsItems.BRONZE.getIngot(), 9)
+            .requires(TCIntegrationsItems.BRONZE.getBlockItemTag())
+            .unlockedBy("has_bronze_ingot", has(TCIntegrationsItems.BRONZE.getIngotTag()))
+            .save(consumer, new ResourceLocation(TCIntegrations.MODID, "bronze_ingots_from_block"));
     }
 
     private static Consumer<FinishedRecipe> withCondition(Consumer<FinishedRecipe> consumer, ICondition... conditions) {
